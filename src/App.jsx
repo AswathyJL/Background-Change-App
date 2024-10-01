e9b59954395d1430e96928309063055e0800bf78
AswathyJL <aswathyjlal@gmail.com>
2024-10-01 20:20:30 +0530

    Background change app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import React from 'react'
+import './App.css'
+import { useState } from 'react'
+
+const App = () => {
+    const [changeColor, setChangeColor] = useState('white')
+
+    // Function to change background color
+    const handleColorChange = (color) => {
+        setChangeColor(color);
+    }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{height:'100vh', backgroundColor: changeColor}} className='d-flex justify-content-center align-items-center'>
+        <div style={{height:'200px', width:'700px'}} className='center-div d-flex flex-column justify-content-evenly align-items-center rounded-5 shadow'>
+            <h1 style={{fontSize:'3rem', fontWeight:'600'}} className='text-primary-emphasis'>Lets Change The Background Color</h1>
+            <div className='d-flex justify-content-evenly align-items-center w-100'>
+                <button onClick={() => handleColorChange('#FEB1A9')} style={{color:'#FEB1A9', fontSize:'2.5rem'}} className='btn'><i className="fa-solid fa-circle shadow rounded-circle border border-secondary border-3"></i></button>
+                <button onClick={() => handleColorChange('#BFB6AE')} style={{color:'#BFB6AE', fontSize:'2.5rem'}} className='btn'><i className="fa-solid fa-circle shadow rounded-circle border border-secondary border-3"></i></button>
+                <button onClick={() => handleColorChange('#FEF2CD')} style={{color:'#FEF2CD', fontSize:'2.5rem'}} className='btn'><i className="fa-solid fa-circle shadow rounded-circle border border-secondary border-3"></i></button>
+                <button onClick={() => handleColorChange('#CFF2B8')} style={{color:'#CFF2B8', fontSize:'2.5rem'}} className='btn'><i className="fa-solid fa-circle shadow rounded-circle border border-secondary border-3"></i></button>
+                <button onClick={() => handleColorChange('#A6D8DB')} style={{color:'#A6D8DB', fontSize:'2.5rem'}} className='btn'><i className="fa-solid fa-circle shadow rounded-circle border border-secondary border-3"></i></button>
+                <button onClick={() => handleColorChange('#BD9CF9')} style={{color:'#BD9CF9', fontSize:'2.5rem'}} className='btn'><i className="fa-solid fa-circle shadow rounded-circle border border-secondary border-3"></i></button>
+            </div>
+        </div>
+    </div>
   )
 }
 
